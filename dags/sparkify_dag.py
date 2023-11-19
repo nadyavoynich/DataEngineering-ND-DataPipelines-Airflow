44fcbdbe250c28add6d7cf94b91f5026f82a320b
Nadya Voynich <nadya.voynich@gmail.com>
2023-11-19 20:15:20 +0100

    Configured the DAG

--- dags/sparkify_dag.py
+++ dags/sparkify_dag.py
@@ -1,18 +1,23 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from operators import (StageToRedshiftOperator, LoadFactOperator,
-                            LoadDimensionOperator, DataQualityOperator)
+                       LoadDimensionOperator, DataQualityOperator)
 
 # AWS_KEY = os.environ.get('AWS_KEY')
 # AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
-    'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
+    'owner': 'sparkify',
+    'start_date': datetime.now(),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False,
 }
 
-dag = DAG('udac_example_dag',
+dag = DAG('sparkify_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
           schedule_interval='0 * * * *'
@@ -61,3 +66,12 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+# Configure the task dependencies
+start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
+[stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+load_songplays_table >> [load_song_dimension_table, load_user_dimension_table,
+                         load_artist_dimension_table, load_time_dimension_table]
+[load_song_dimension_table, load_user_dimension_table,
+ load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
+run_quality_checks >> end_operator
