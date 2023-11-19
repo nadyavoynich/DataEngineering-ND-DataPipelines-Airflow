65e34aad4c3f21a158192f5e1ae59c40442d4056
Nadya Voynich <nadya.voynich@gmail.com>
2023-11-19 22:56:56 +0100

    Built Stage operator

--- dags/operators/stage_redshift.py
+++ dags/operators/stage_redshift.py
@@ -1,22 +1,49 @@
-# from airflow.hooks.postgres_hook import PostgresHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+    template_fields = ("s3_key",)
+    copy_sql = """
+    COPY {}
+    FROM '{}'
+    CREDENTIALS 'aws_iam_role={}'
+    REGION '{}'
+    JSON '{}'
+    """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_role_arn="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 json="",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_role_arn = aws_role_arn
+        self.table = table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.json = json
 
     def execute(self, context):
         self.log.info('StageToRedshiftOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info('Copying data from S3 to Redshift')
+
+        rendered_key = self.s3_key.format(**context)
+        s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
+        formatted_sql = StageToRedshiftOperator.copy_sql.format(
+            self.table,
+            s3_path,
+            self.aws_role_arn,
+            self.json
+        )
+
+        redshift.run(formatted_sql)
